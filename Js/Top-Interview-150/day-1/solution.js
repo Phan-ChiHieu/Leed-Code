d04d6117d78d0eb7c837ef0ec1724b2f50dfd974
EdricPhan <edricphan.dev@gmail.com>
2023-11-14 09:52:44 +0700

    update code

---
+++ Js/Top-Interview-150/day-1/solution.js
@@ -0,0 +1,31 @@
+/*
+Example 1:
+
+    Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+    Output: [1,2,2,3,5,6]
+    Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
+    The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
+   
+Example 2:
+
+    Input: nums1 = [1], m = 1, nums2 = [], n = 0
+    Output: [1]
+    Explanation: The arrays we are merging are [1] and [].
+    The result of the merge is [1].
+
+Example 3:
+
+    Input: nums1 = [0], m = 0, nums2 = [1], n = 1
+    Output: [1]
+    Explanation: The arrays we are merging are [] and [1].
+    The result of the merge is [1].
+    Note that because m = 0, there are no elements in nums1. The 0 is only there to ensure the merge result can fit in nums1.
+
+*/
+
+var merge = function (nums1, m, nums2, n) {
+  for (let i = m, j = 0; j < n; i++, j++) {
+    nums1[i] = nums2[j];
+  }
+  nums1.sort((a, b) => a - b);
+};
